0cc6ee778b19628cc9f556f1351f2f9dc4da588c
alex <o.postovyi@gmail.com>
2026-02-22 00:01:06 +0200

    feat(chain-of-knowledge): add CoK prompt template

---
+++ construct/thought/cok.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+from construct.base import PromptSection
+from construct.zero_shot import ZeroShotPrompt
+
+@dataclass
+class Triplet:
+    items: list[str]
+
+    def __str__(self) -> str:
+        return str(self.items)
+
+class ChainOfKnowledge(ZeroShotPrompt):
+    def __init__(self, instructions: PromptSection, knowledge_triplets: list[Triplet], explanation: PromptSection) -> None:
+        super().__init__(instructions)
+        self.add_section(name="knowledge_triplets", section=PromptSection(content="\n\n".join([str(triplet) for triplet in knowledge_triplets])))
+        self.add_section(name="explanation", section=explanation)
+
